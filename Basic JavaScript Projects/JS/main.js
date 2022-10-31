b923dd1c7ad0f5061e3cf18575fe40afe6d7aada
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2022-10-31 14:21:52 -0600

    First submission for Basic JavaScript

---
+++ Basic JavaScript Projects/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, World!");
+
+var x = "and this is the end of the string \\backslash and \"qutation\" marks."; // Setting a string variable to the following text
+var y = "and this is the end of the string "; // Setting a string variable to the following text
+var sentence = "\n This string will be concatenated with x and y" // Setting a string variable to the following text
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro"; // Setting a string variable to the following String array with key/values
+
+document.write(x.concat(y)); // printing and concatenating x and y string variables to the document body
+document.write(sentence.concat(x+y)); // printing and concatenating sentence, x and y string variables to the document body
+
+document.write(Mom); // printing string value assigned to Mom to the document body
+
+document.write(3+3); // printing integer values 3 + 3 to the document body which displays as 6
